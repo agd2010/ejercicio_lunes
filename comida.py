e21c706072e27ff4cb8b27521fbcff112542c0b7
agd2010 <andresgdati@gmail.com>
2023-11-23 12:18:25 -0300

    Update: add simple sample code

--- comida.py
+++ comida.py
@@ -1,4 +1,13 @@
 class Comida():
-    def __init__(self) -> None:
-        pass
-    
+    def __init__(self, salado_dulce: str) -> str:
+        self.salado_dulce = salado_dulce
+
+        if (self.salado_dulce == "dulce") or (self.salado_dulce == "salado"):
+                print(f"Su selección fue {self.salado_dulce.capitalize()}")
+        else:   
+                print(f"Error en la selección, debe indicar Dulce o Salado")
+
+
+seleccion = input("Seleccione Dulce o Salado: ")
+seleccion = seleccion.lower()
+Comida(seleccion)
